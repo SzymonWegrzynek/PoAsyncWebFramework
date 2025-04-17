b00570ccb56f5cb085bc2a6a99f8ee0e2097d5d7
Szymon Wegrzynek <u105_szyweg_waw@technischools.com>
2025-04-17 13:19:45 +0200

    little changes

--- client/src/cli.rs
+++ client/src/cli.rs
@@ -1,2 +1,5 @@
-// odczytywanie tekstu z pliku i zapis  program -rf <sciezka> -s <nazwa_pliku> -d <sciezka>
-// odczytywanie tekstu z konsoli  program -rc <tekst>
+// odczytywanie tekstu z pliku i zapis  program -rf <sciezka> -s <nazwa_pliku> -v <wartość>
+// odczytywanie tekstu z konsoli  program -rt <tekst> -s <nazwa_pliku> -v <wartość>
+// za każdym razem tekst jest wypowiadany
+// plik z dźwiękiem jest zapisywany do folderu pobrane
+// -v to volume
